106a50f033f9911c417a0b4fabbdd90076f5d2a0
parham <parhammm76@gmail.com>
2023-12-02 20:27:01 +0330

    first commit

---
+++ src/home/home.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { HomeService } from './home.service';
+import { HomeResponseDto } from './dto/home.dto';
+
+@Controller('home')
+export class HomeController {
+  constructor(private readonly homeService: HomeService) {}
+  @Get()
+  getHomes(): Promise<HomeResponseDto[]> {
+    return this.homeService.getHomes();
+  }
+  @Get(':id')
+  getHome() {
+    return {};
+  }
+  @Post()
+  createHome() {
+    return {};
+  }
+  @Put(':id')
+  updateHome() {
+    return {};
+  }
+
+  @Delete(':id')
+  deleteHome() {
+    console.log('delete');
+  }
+}
