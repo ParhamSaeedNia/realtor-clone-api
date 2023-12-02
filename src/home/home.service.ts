f7b14452654edfe8607e68b4f9819777f14516ef
parham <parhammm76@gmail.com>
2023-12-02 20:32:09 +0330

    first commit

--- src/home/home.service.ts
+++ src/home/home.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { HomeResponseDto } from './dto/home.dto';
+import { PropertyType } from '@prisma/client';
+
+interface GetHomesParam {
+  city?: string;
+  price?: {
+    gte?: number;
+    lte?: number;
+  };
+  propertyType?: PropertyType;
+}
 
 @Injectable()
 export class HomeService {
   constructor(private readonly prismaService: PrismaService) {}
-  async getHomes(): Promise<HomeResponseDto[]> {
+  async getHomes(filter: GetHomesParam): Promise<HomeResponseDto[]> {
     const homes = await this.prismaService.home.findMany({
       select: {
         id: true,
@@ -23,6 +33,7 @@ export class HomeService {
           take: 1,
         },
       },
+      where: filter,
     });
     return homes.map((home) => {
       const fetchHome = { ...home, image: home.images[0].url };
