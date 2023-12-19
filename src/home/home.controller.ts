d27809d013aedf0db92d662eb148310cc3881358
parham <parhammm76@gmail.com>
2023-12-19 18:26:33 +0330

    Adding authorization

--- src/home/home.controller.ts
+++ src/home/home.controller.ts
@@ -12,8 +12,9 @@ import {
 } from '@nestjs/common';
 import { HomeService } from './home.service';
 import { CreateHomeDto, HomeResponseDto, UpdateHomeDto } from './dto/home.dto';
-import { PropertyType } from '@prisma/client';
+import { PropertyType, UserType } from '@prisma/client';
 import { User, UserInfo } from 'src/user/decorators/user.decorator';
+import { Roles } from 'src/decorators/roles.decorator';
 
 @Controller('home')
 export class HomeController {
@@ -46,10 +47,12 @@ export class HomeController {
   getHome(@Param('id', ParseIntPipe) id: number) {
     return this.homeService.getHomeById(id);
   }
+  @Roles(UserType.REALTOR)
   @Post()
   createHome(@Body() body: CreateHomeDto, @User() user: UserInfo) {
     return this.homeService.createHome(body, user.id);
   }
+  @Roles(UserType.REALTOR)
   @Put(':id')
   async updateHome(
     @Param('id', ParseIntPipe) id: number,
@@ -60,7 +63,7 @@ export class HomeController {
     if (realtor.id !== user.id) throw new UnauthorizedException();
     return this.homeService.updateHomeById(id, body);
   }
-
+  @Roles(UserType.REALTOR)
   @Delete(':id')
   async deleteHome(
     @Param('id', ParseIntPipe) id: number,
